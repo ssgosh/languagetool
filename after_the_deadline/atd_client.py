a89d634faafc5a652f48a04b0eb8f6d2109b41b4
Sabyasachi Ghosh <ssgosh@gmail.com>
2018-09-17 00:44:02 +0530

    Adding After The Deadline python bindings and client

---
+++ after_the_deadline/atd_client.py
@@ -0,0 +1,22 @@
+import ATD
+import sys
+
+ATD.setDefaultKey("GEC_f5d029b602c7b98794329473432")
+
+#sentences = ["Looking too the water. Fixing your writing typoss.",
+#	"THis sentence is corrupt. Or is it? Who knows, really? Please test it out. Bruno is an good dog. Good dogs is rare."]
+
+if len(sys.argv) < 2:
+    print "Need input filename"
+    sys.exit(1)
+
+filename = sys.argv[1]
+f = open(filename, "r")
+sentences = f.readlines()
+
+for s in sentences:
+    errors = ATD.checkDocument(s)
+    for error in errors:
+        print "%s error for: %s **%s**" % (error.type, error.precontext, error.string)
+        print "some suggestions: %s" % (", ".join(error.suggestions),)
+
