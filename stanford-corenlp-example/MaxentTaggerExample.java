cc212b807b6dbdd6187ae1fdedef78b1268ff19d
Sabyasachi Ghosh <ssgosh@gmail.com>
2018-10-06 00:18:31 +0530

    Parse postags from tagged sentence

--- stanford-corenlp-example/MaxentTaggerExample.java
+++ stanford-corenlp-example/MaxentTaggerExample.java
@@ -6,7 +6,14 @@ public class MaxentTaggerExample {
     //MaxentTagger tagger = new MaxentTagger("lib/tagger/taggers/english-left3words-distsim.tagger");
     MaxentTagger tagger = new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
     String tags = tagger.tagString("I am a good boy.");
-    System.out.println(tags);
+    //System.out.println(tags);
+    String[] eachTag = tags.split("\\s+");
+
+    for (String str : eachTag) {
+        int i = str.lastIndexOf("_");
+        String postag = str.substring(i + 1);
+        System.out.println(postag);
+    }
   }
 }
 
