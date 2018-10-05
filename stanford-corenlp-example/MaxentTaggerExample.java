351124a64cb88646797b3601e5c3d5ccb225ca31
Sabyasachi Ghosh <ssgosh@gmail.com>
2018-10-06 01:16:43 +0530

    Test tagging list of words

--- stanford-corenlp-example/MaxentTaggerExample.java
+++ stanford-corenlp-example/MaxentTaggerExample.java
@@ -1,19 +1,54 @@
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import java.util.*;
 
 public class MaxentTaggerExample {
+
+  public static String concatStringsWSep(Iterable<String> strings, String separator) {
+    StringBuilder sb = new StringBuilder();
+    String sep = "";
+    for(String s: strings) {
+      sb.append(sep).append(s);
+      sep = separator;
+    }
+    return sb.toString();                           
+  }
+
   public static void main(String []args) {
     //MaxentTagger tagger = new MaxentTagger("english-left3words-distsim.tagger");
     //MaxentTagger tagger = new MaxentTagger("lib/tagger/taggers/english-left3words-distsim.tagger");
+    String [] words = {"He", "has", "a", "dog"};
+    List<String> sentence = Arrays.asList(words);
+    String str_sentence = concatStringsWSep(sentence, " ");
+    System.out.println(str_sentence);
+    
+    List<String> tags = tagString(str_sentence);
+    for (String tag: tags) {
+        System.out.println(tag);
+    }
+
+    tags = tagString("I am a good boy");
+    for (String tag: tags) {
+        System.out.println(tag);
+    }
+
+  }
+
+  public static List<String> tagString(String sentence) {
     MaxentTagger tagger = new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
-    String tags = tagger.tagString("I am a good boy.");
+
+    String tags = tagger.tagString(sentence);
     //System.out.println(tags);
     String[] eachTag = tags.split("\\s+");
 
+    List<String> postags = new ArrayList<>();
     for (String str : eachTag) {
         int i = str.lastIndexOf("_");
         String postag = str.substring(i + 1);
-        System.out.println(postag);
+        //System.out.print(postag + " ");
+        postags.add(postag);
     }
+
+    return postags;
   }
 }
 
